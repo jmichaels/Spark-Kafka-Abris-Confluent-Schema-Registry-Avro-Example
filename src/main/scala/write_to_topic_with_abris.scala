0ae21e3df39ed65f4413243b2b9039b94e52463c
John Michaels <johnrmichaelsjr@gmail.com>
2021-04-21 20:16:48 -0400

    Add giant, super important comment about nullability
    Yes, I know - this probably belongs in the README...
    ...I'll get to it...eventually...

--- src/main/scala/write_to_topic_with_abris.scala
+++ src/main/scala/write_to_topic_with_abris.scala
@@ -13,6 +13,57 @@ object WriteToTopicWithAbris {
       .master("local[*]")
       .getOrCreate()
 
+    // Nullability is important.
+    //
+    // The nullability of your Schema Registry schema fields
+    // must match the nullability of your dataframe fields, or
+    // you will get a confusing error like:
+    //
+    // org.apache.avro.AvroRuntimeException: Not a union: "int"
+    //
+    // Unfortunately, setting nullable as "false" here, does
+    // not actually do anything.  The dataframe schema will still
+    // say the fields are "nullable: True".
+    //
+    // See:  https://stackoverflow.com/questions/41705602/spark-dataframe-schema-nullable-fields
+    //
+    // Not sure how to solve this at the moment, but for testing
+    // you can change the Schema Registry schema for this topic
+    // so that all fields allow null:
+    //
+    // {
+    //   "doc": "Sample schema to help you get started.",
+    //   "fields": [
+    //     {
+    //       "doc": "The int type is a 32-bit signed integer.",
+    //       "name": "field1",
+    //       "type": [
+    //         "int",
+    //         "null"
+    //       ]
+    //     },
+    //     {
+    //       "doc": "The double type is a double precision (64-bit) IEEE 754 floating-point number.",
+    //       "name": "field2",
+    //       "type": [
+    //         "double",
+    //         "null"
+    //       ]
+    //     },
+    //     {
+    //       "doc": "The string is a unicode character sequence.",
+    //       "name": "field3",
+    //       "type": [
+    //         "string",
+    //         "null"
+    //       ]
+    //     }
+    //   ],
+    //   "name": "value_abris_test",
+    //   "namespace": "com.mycorp.mynamespace",
+    //   "type": "record"
+    // }
+    //
     val schema = new StructType()
       .add("field1", IntegerType, false)
       .add("field2", DoubleType, false)
